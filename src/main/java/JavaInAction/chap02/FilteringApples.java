1e69c1605a122e54c19872789788c8a87e56839a
jm <rjm9303@gmail.com>
2021-10-04 13:23:44 +0900

    chap - 2 복습 summary 추가

--- src/main/java/JavaInAction/chap02/FilteringApples.java
+++ src/main/java/JavaInAction/chap02/FilteringApples.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 public class FilteringApples {
-
-    enum Color {RED, GREEN}
 
     public static void main(String[] args) {
 
@@ -17,26 +16,29 @@ public class FilteringApples {
             new Apple(79, Color.RED), new Apple(81, Color.RED), new Apple(151, Color.GREEN),
             new Apple(149, Color.GREEN), new Apple(151, Color.RED), new Apple(149, Color.RED));
 
-        //        List<Apple> greenApples = filterApplesByColor(inventory, Color.GREEN);
+        //List<Apple> greenApples = filterApplesByColor(inventory, Color.GREEN);
 
         List<Apple> heavyApples = filterApples(inventory, new AppleHeavyWeightPredicate());
-        System.out.println(heavyApples);
+        System.out.println("heavyApples = " + heavyApples);
 
         List<Apple> greenApples = filterApples(inventory, new AppleGreenColorPredicate());
-        System.out.println(greenApples);
+        System.out.println("greenApples = " + greenApples);
 
         List<Apple> redAndLightApples = filterApples(inventory, new AppleRedAndLightPredicate());
-        System.out.println(redAndLightApples);
+        System.out.println("redAndLightApples = " + redAndLightApples);
+
+        List<Apple> redAndHeavyApples = filterApples(inventory, new AppleRedAndHeavyPredicate());
+        System.out.println("redAndHeavyApples = " + redAndHeavyApples);
+
+        List<Apple> greenAndLightApples = filterApples(inventory,
+            apple -> Color.GREEN == apple.getColor() && apple.getWeight() < 150);
+        System.out.println("greenAndLightApples = " + greenAndLightApples);
 
         prettyPrintApple(inventory, new WeightOfAppleFormatter());
 
         inventory.sort(Comparator.comparing(Apple::getWeight));
         System.out.println(inventory);
-    }
 
-    public interface ApplePredicate {
-
-        boolean test(Apple apple);
     }
 
     public static void prettyPrintApple(List<Apple> inventory, AppleFormatter formatter) {
@@ -44,6 +46,32 @@ public class FilteringApples {
             String output = formatter.accept(apple);
             System.out.println(output);
         }
+    }
+
+    // stream 사용으로 refactoring
+    // result 변수를 만들 필요 없고 반복 과정을 직접 제어하지 않아도 된다 무엇을 할지 명시해줌
+    public static List<Apple> filterApples(List<Apple> inventory, ApplePredicate p) {
+        return inventory.stream()
+            .filter(p::test)
+            .collect(Collectors.toList());
+    }
+
+    // refactoring 이전 버전
+    public static List<Apple> filterApplesByColor(List<Apple> inventory, Color color) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (apple.getColor().equals(color)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    enum Color {RED, GREEN}
+
+    public interface ApplePredicate {
+
+        boolean test(Apple apple);
     }
 
     public interface AppleFormatter {
@@ -71,7 +99,15 @@ public class FilteringApples {
 
         @Override
         public boolean test(Apple apple) {
-            return Color.GREEN.equals(apple.getColor());
+            return Color.GREEN == apple.getColor();
+        }
+    }
+
+    static class AppleRedAndHeavyPredicate implements ApplePredicate {
+
+        @Override
+        public boolean test(Apple apple) {
+            return Color.RED == apple.getColor() && apple.getWeight() > 150;
         }
     }
 
@@ -79,36 +115,16 @@ public class FilteringApples {
 
         @Override
         public boolean test(Apple apple) {
-            return Color.RED.equals(apple.getColor()) && apple.getWeight() < 150;
+            return Color.RED == apple.getColor() && apple.getWeight() < 150;
         }
-    }
-
-    public static List<Apple> filterApples(List<Apple> inventory, ApplePredicate p) {
-        List<Apple> result = new ArrayList<>();
-        for (Apple apple : inventory) {
-            if (p.test(apple)) {
-                result.add(apple);
-            }
-        }
-        return result;
     }
 
     @Data
     @AllArgsConstructor
     public static class Apple {
 
-        private int weight = 0;
+        private Integer weight = 0;
         private Color color;
-    }
-
-    public static List<Apple> filterApplesByColor(List<Apple> inventory, Color color) {
-        List<Apple> result = new ArrayList<>();
-        for (Apple apple : inventory) {
-            if (apple.getColor().equals(color)) {
-                result.add(apple);
-            }
-        }
-        return result;
     }
 
 }
---
+++ src/main/java/JavaInAction/chap02/Summary.java
@@ -0,0 +1,73 @@
+package JavaInAction.chap02;
+
+import JavaInAction.chap02.FilteringApples.Apple;
+import JavaInAction.chap02.FilteringApples.Color;
+import java.awt.Button;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Summary {
+
+    private static List<Apple> inventory = Arrays.asList(new Apple(79, Color.GREEN), new Apple(81, Color.GREEN),
+        new Apple(79, Color.RED), new Apple(81, Color.RED), new Apple(151, Color.GREEN),
+        new Apple(149, Color.GREEN), new Apple(151, Color.RED), new Apple(149, Color.RED));
+
+    public static void main(String[] args) {
+        final List<Integer> integers = List.of(1, 2, 3, 4, 5, 6);
+        final List<Integer> filteredIntegers = filter(integers, integer -> integer > 3);
+        System.out.println("filteredIntegers = " + filteredIntegers);
+
+
+        // compareTo 쓰려면 변수가 Integer 형이어야 한다
+        // inventory.sort((a1, a2) -> a1.getWeight().compareTo(a2.getWeight()));
+        // 요렇게 축약도 가능하구나
+        inventory.sort(Comparator.comparing(Apple::getWeight));
+        System.out.println("inventory = " + inventory);
+
+
+        /*
+        final Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("yaho");
+            }
+        });
+        */
+
+        // t1.run(); -> t1.start(); 바꾸라고 알려준당
+        final Thread t1 = new Thread(() -> System.out.println("yaho!"));
+        t1.start();
+
+        // Callable 은 Runnable 의 업그레이드 버전?
+        final ExecutorService executorService = Executors.newCachedThreadPool();
+        /*
+        final Future<String> threadName = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return Thread.currentThread().getName();
+            }
+        });
+        */
+
+        // 람다 이용!
+        final Future<String> threadName = executorService.submit(() -> Thread.currentThread().getName());
+        System.out.println("threadName = " + threadName);
+    }
+
+    private static <T> List<T> filter(List<T> list, Predicate<T> p) {
+        return list.stream()
+            .filter(p)
+            .collect(Collectors.toList());
+    }
+}
+
+/*
+T로 추상화해 List 형식에 적용될 수 있도록 캬!
+ */
